57fb18c502d795bb39188379a7a9bb340f5cad60
Matt Bernhardt <mjbernha@mit.edu>
2019-05-23 19:42:20 -0400

    Updates catalog packages for python3

--- catalog.py
+++ catalog.py
@@ -1,62 +1,62 @@
-"""This module provides a class Catalog, representing the OPAC.""" 
-
-import urllib, urllib2, cookielib
-import settings
-
-class Catalog:
-    """This class handles queries to the OPAC."""
-
-    def __init__(self):
-        """Init login URL, credentials, cookies, and URL opener."""
-
-        # Login URL and settings
-        self.loginurl = settings.LOGIN_URL
-        self.credentials = {
-                settings.USERNAME_LABEL: settings.USERNAME,
-                settings.PASSWORD_LABEL: settings.PASSWORD}
-
-        # Make the cookie jar
-        self.cookies = cookielib.CookieJar()
-
-        # Build URL opener
-        self.opener = urllib2.build_opener(
-                urllib2.HTTPRedirectHandler(),
-                urllib2.HTTPHandler(),
-                urllib2.HTTPSHandler(),
-                urllib2.HTTPCookieProcessor(self.cookies))
-
-    def login(self):
-        """Log into the OPAC."""
-
-        # Open the page
-        self.opener.open(self.loginurl)
-
-        # Create request object
-        request = urllib2.Request(self.loginurl, 
-                urllib.urlencode(self.credentials))
-
-        # Log in
-        return self.opener.open(request)
-
-    def is_logged_in(self):
-        """Check if logged in to catalog."""
-
-        # Get account page
-        page = self.opener.open(settings.ACCOUNT_URL).read()
-
-        # Check account page text for login name
-        return settings.LOGIN_SUCCESS in page
-
-    def search_barcode(self, barcode):
-        """Search for item by barcode and return HTML."""
-
-        # Search for barcode
-        page = self.opener.open(
-                settings.BARCODE_URL + barcode).read()
-        if "Bib Level" in page:
-            return page
-        else:
-            self.login()
-            page = self.opener.open(
-                    settings.BARCODE_URL + barcode).read()
-            return page
+"""This module provides a class Catalog, representing the OPAC.""" 
+
+import urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse, http.cookiejar
+import settings
+
+class Catalog:
+    """This class handles queries to the OPAC."""
+
+    def __init__(self):
+        """Init login URL, credentials, cookies, and URL opener."""
+
+        # Login URL and settings
+        self.loginurl = settings.LOGIN_URL
+        self.credentials = {
+                settings.USERNAME_LABEL: settings.USERNAME,
+                settings.PASSWORD_LABEL: settings.PASSWORD}
+
+        # Make the cookie jar
+        self.cookies = http.cookiejar.CookieJar()
+
+        # Build URL opener
+        self.opener = urllib.request.build_opener(
+                urllib.request.HTTPRedirectHandler(),
+                urllib.request.HTTPHandler(),
+                urllib.request.HTTPSHandler(),
+                urllib.request.HTTPCookieProcessor(self.cookies))
+
+    def login(self):
+        """Log into the OPAC."""
+
+        # Open the page
+        self.opener.open(self.loginurl)
+
+        # Create request object
+        request = urllib.request.Request(self.loginurl, 
+                urllib.parse.urlencode(self.credentials))
+
+        # Log in
+        return self.opener.open(request)
+
+    def is_logged_in(self):
+        """Check if logged in to catalog."""
+
+        # Get account page
+        page = self.opener.open(settings.ACCOUNT_URL).read()
+
+        # Check account page text for login name
+        return settings.LOGIN_SUCCESS in page
+
+    def search_barcode(self, barcode):
+        """Search for item by barcode and return HTML."""
+
+        # Search for barcode
+        page = self.opener.open(
+                settings.BARCODE_URL + barcode).read()
+        if "Bib Level" in page:
+            return page
+        else:
+            self.login()
+            page = self.opener.open(
+                    settings.BARCODE_URL + barcode).read()
+            return page
